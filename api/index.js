f30954148b38fcf41aca57e093bf671a03d9aa23
DeanVini <deanvinicius2003@gmail.com>
2025-08-11 23:25:05 -0300

    refactor: simplify NestJS app initialization and update Vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,37 +1,19 @@
 const { NestFactory } = require('@nestjs/core');
 const { AppModule } = require('../dist/app.module');
-const { ValidationPipe } = require('@nestjs/common');
 
-let cachedApp;
+let app;
 
 async function createApp() {
-  if (cachedApp) {
-    return cachedApp;
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
   }
-
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.init();
-  cachedApp = app.getHttpAdapter().getInstance();
-  return cachedApp;
+  return app;
 }
 
 module.exports = async (req, res) => {
-  try {
-    const app = await createApp();
-    return app(req, res);
-  } catch (error) {
-    console.error('Serverless function error:', error);
-    res.status(500).json({
-      error: 'Internal Server Error',
-      message: error.message
-    });
-  }
+  const server = await createApp();
+  const expressApp = server.getHttpAdapter().getInstance();
+  return expressApp(req, res);
 };
